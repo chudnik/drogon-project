440a1182a5470d5f449c3c2abca44ef18fa76b23
chudnik <chud.nikita548@yandex.ru>
2026-07-22 18:20:58 +0300

    Добавлен основной файл main.cc с конфигурацией сервера и инициализацией приложения

---
+++ main.cc
@@ -0,0 +1,15 @@
+#include <drogon/drogon.h>
+
+#include "controllers/task_controller.h"
+#include "filters/api_key_filter.h"
+#include "plugins/request_timer_plugin.h"
+#include "websockets/task_web_socket.h"
+
+using namespace drogon;
+
+int main() {
+    app().loadConfigFile("config.json");
+    LOG_INFO << "Server starting on http://0.0.0.0:8080";
+    app().run();
+    return 0;
+}
